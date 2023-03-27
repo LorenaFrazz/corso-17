f21c058926d5b33810f330de1cab8de00c0dd03c
LorenaFrazz <frazzetta.lorena@hotmail.com>
2023-03-27 10:00:47 +0200

    feat(): add product callback

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 //Start project
 
+/*
+through type, choose add or minus
+*/
 const add = (a: number, b: number) => a + b;
 
 const mins = (a: number, b: number) => a - b;
+
+const genericFunction = (a: number, b: number, type: '+' | '-') =>
+type === '+' ? a + b : a - b 
